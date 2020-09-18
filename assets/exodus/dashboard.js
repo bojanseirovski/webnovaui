da8145a732e0ae9fe11e9248f743212a9cef3f39
Dennis <d_silin@yahoo.com>
2020-09-17 21:27:20 -0400

    Changed the call convention for sim_step a bit

--- assets/exodus/dashboard.js
+++ assets/exodus/dashboard.js
@@ -1,5 +1,5 @@
 /**
- * Map width/360 or Map heoight/180 
+ * Map width/360 or Map heoight/180
  * 298/360 , 150/360
  */
 var mapConversionConstX = 0.8277;
@@ -12,11 +12,11 @@ var mapHeight = 150;
 /**
  * Pause between AJAX calls
  */
-var loopBreak = 3500;
+var loopBreak = 3000;
 var baseUrl = "http://127.0.0.1:8000"
 
 var norad_url = "";
-var sim_step_url = "?steps=";
+var sim_step_url = "?steps=3"; // in seconds
 var sim_steps = 1;
 
 var app = new Vue({
@@ -51,9 +51,10 @@ var app = new Vue({
 	mounted: function () {
 		var theApp = this;
 		this.getNoradId();
-		setTimeout(() => {  console.log("pause"); }, 3000);
+    setTimeout(() => {  console.log("pause"); }, 3000);
 		this.interval = setInterval(function () {
 			theApp.simStep();
+      //console.log(window.sessionStorage);
 			// theApp.showLocation();
 			// theApp.getTelemetry();
 			// theApp.getLog();
@@ -75,10 +76,9 @@ var app = new Vue({
 			this.loadApi(this.api.sim_init + norad_url+"&date="+timeNow, function (data) { });
 		},
 		simStep() {
-			this.loadApi(this.api.sim_step + sim_steps, function (data) {
-				
+			this.loadApi(this.api.sim_step, function (data) {
+
 			 });
-			 sim_steps++;
 		},
 		getLog() {
 			this.loadApi(this.api.log + norad_url, function (data) {
