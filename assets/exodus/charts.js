eff18f44eb3cde75f96826552dba10e050c1d50e
Bojan Seirovski <druid0101@gmail.com>
2021-10-25 01:22:09 -0400

    object recognition

---
+++ assets/exodus/charts.js
@@ -0,0 +1,33 @@
+const ctx4 = document.getElementById('chart').getContext('2d');
+const myChart = new Chart(ctx4, {
+    type: 'bar',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [{
+            label: '# of objects detected',
+            data: [0, 1, 1, 2, 1, 0],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
